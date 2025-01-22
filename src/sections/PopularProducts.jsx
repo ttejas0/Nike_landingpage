a485bd3bb5ba6be195dddc21efd04742b9e054c6
Tejas <ttejas220@gmail.com>
2025-01-22 11:17:51 +0530

    popular products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,23 @@
+import { products } from "../constants"
+import PopularProductCard from "../components/PopularProductCard"
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="products"
+    className="max-container max-sm:mt-12"
+    >
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Immerse yourself in unmatched quality and sophistication with our premium collections. Explore a realm of elegance, comfort, and exceptional value.</p>
+
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+          {products.map((product) => (
+            <PopularProductCard key={product.name} {...product} />
+          ))}
+        </div>
+
+      </div>
+    </section>
   )
 }
 
